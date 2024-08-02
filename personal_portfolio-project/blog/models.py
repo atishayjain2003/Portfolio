05d437f06dcac0f8cf473bdb7a77c68f00702ad6
atishayjain2003 <atishayjain2003@gmail.com>
2024-08-03 04:56:54 +0530

    Seeting up the home page

--- personal_portfolio-project/blog/models.py
+++ personal_portfolio-project/blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Blog(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    date=models.DateField()
+
 # Create your models here.
