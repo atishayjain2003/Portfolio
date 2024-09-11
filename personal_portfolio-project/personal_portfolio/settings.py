7a4188f41a4f87d5e2fb5ed4e1247664248f69a3
atishayjain2003 <atishayjain2003@gmail.com>
2024-09-11 18:47:41 +0530

    Updated settings

--- personal_portfolio-project/personal_portfolio/settings.py
+++ personal_portfolio-project/personal_portfolio/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2*i3s(#p&q8f_cd8+(r4u@4z+mr01l-kwwyn9^jl+28hk4zu%6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['atttyshay.pythonanywhere.com']
 
 
 
